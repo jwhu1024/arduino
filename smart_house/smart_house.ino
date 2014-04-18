1f0cf7e0c5cd3e149d87d991f33c821847116686
lester <lester_hu@bandrich.com>
2014-04-18 16:20:34 +0800

    add door handler with dummy data

--- smart_house/smart_house.ino
+++ smart_house/smart_house.ino
@@ -10,7 +10,9 @@
 #define LED_OLD_STATE_IDX   2
 #define MAX_PATH            16
 
-const char *led_path = "/tmp";
+static const char *led_path    = "/tmp";
+static const char *door_sh_path   = "/demo.sh";
+
 int ledPins[][3] =
 {
     /* Pin  , New stat, Old state*/
@@ -36,14 +38,14 @@ void setup() {
     // Wait until a Serial Monitor is connected.
     while (!Serial);
 
-    Serial.println(led_tbl_sz);
+    randomSeed(analogRead(0));
 }
 
 void led_update(int idx, int ret)
 {
     ledPins[idx][LED_NEW_STATE_IDX] = (ret == 1) ? LED_ON : LED_OFF;
     if (ledPins[idx][LED_NEW_STATE_IDX] == ledPins[idx][LED_OLD_STATE_IDX]) {
-        Serial.println("Status NOT change, do nothing");
+        // Serial.println("Status NOT change, do nothing");
         return;
     }
     
@@ -77,16 +79,39 @@ void runLedHandler()
             // LED Control
             led_update(idx, proc.parseInt());
         } else {
-            Serial.println("Command Failed without return value");
+            // Serial.println("Command Failed without return value");
         }
 
-        Serial.flush();
+        // Serial.flush();
         proc.flush();
         memset(fp, 0, MAX_PATH);
     }
 }
 
+void runDoorHandler()
+{
+    /* dummy data */
+    int door_inst = random(0, 5);
+    /* dummy data */
+
+    // set path
+    char cmd[MAX_PATH]={0};
+    sprintf(cmd, "%s %d", door_sh_path, door_inst);
+
+    // run cat command
+    Process proc;
+    proc.runShellCommand(cmd);
+
+    // do nothing until the process finishes
+    while (proc.running());
+    
+    // Serial.flush();
+    proc.flush();
+}
+
 void loop() {
     runLedHandler();
-    delay(2000);  // wait 5 seconds before you do it again
+    delay(500);
+    runDoorHandler();
+    delay(500);
 }
