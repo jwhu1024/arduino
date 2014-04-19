2c2e63b3b613a41e76b6961cdf9c7fe33f12b91d
lester_hu <jwhu1024@gmail.com>
2014-04-19 21:51:27 +0800

    replace "process" with "FileIO". code is more clear and more meet our requirements (file read/write)

--- smart_house/smart_house.ino
+++ smart_house/smart_house.ino
@@ -1,8 +1,8 @@
-#include <Process.h>
+#include <FileIO.h>
 
-#define LED_1               11
-#define LED_2               12
-#define LED_3               13
+#define LED_11              11
+#define LED_12              12
+#define LED_13              13
 #define LED_ON              1
 #define LED_OFF             0
 #define LED_PIN_IDX         0
@@ -11,14 +11,14 @@
 #define MAX_PATH            16
 
 static const char *led_path    = "/tmp";
-static const char *door_sh_path   = "/demo.sh";
+static const char *door_path   = "/tmp/door";
 
 int ledPins[][3] =
 {
-    /* Pin  , New stat, Old state*/
-    { LED_1 , LED_OFF , LED_OFF },
-    { LED_2 , LED_OFF , LED_OFF },
-    { LED_3 , LED_OFF , LED_OFF }
+    /* Pin   , New stat, Old state*/
+    { LED_11 , LED_OFF , LED_OFF },
+    { LED_12 , LED_OFF , LED_OFF },
+    { LED_13 , LED_OFF , LED_OFF }
 };
 
 static unsigned int led_tbl_sz = (sizeof(ledPins) / sizeof(ledPins[0]));
@@ -35,14 +35,16 @@ void setup() {
     // Initialize the Serial
     Serial.begin(9600);
 
+    // Setup File IO
+    FileSystem.begin();
+
     // Wait until a Serial Monitor is connected.
     while (!Serial);
 
     randomSeed(analogRead(0));
 }
 
-void led_update(int idx, int ret)
-{
+void led_update(int idx, int ret) {
     ledPins[idx][LED_NEW_STATE_IDX] = (ret == 1) ? LED_ON : LED_OFF;
     if (ledPins[idx][LED_NEW_STATE_IDX] == ledPins[idx][LED_OLD_STATE_IDX]) {
         // Serial.println("Status NOT change, do nothing");
@@ -57,56 +59,46 @@ void led_update(int idx, int ret)
     return;
 }
 
-void runLedHandler()
-{
-    Process proc;
-    char fp[MAX_PATH]={0};
-    
+void read_led_data(char *fp, String &data) {
+    if (true == FileSystem.exists(fp)) {
+        File leds = FileSystem.open(fp, FILE_READ);
+        while (leds.available() > 0) {
+            char c = leds.read();
+            data += c;
+        }
+        leds.close();
+    }
+    return;
+}
+
+void runLedHandler() {
     for (int idx = 0; idx < led_tbl_sz; idx++) {
+        String buf = "";
+        char fp[MAX_PATH]={0};
+
         // set path
         sprintf(fp, "%s/%d", led_path, ledPins[idx][LED_PIN_IDX]);
-        
-        // run cat command
-        proc.begin("cat");
-        proc.addParameter(fp); 
-        proc.run();
-        
-        // do nothing until the process finishes
-        while (proc.running());
-        
-        // Success if exit value is zero
-        if (proc.exitValue() == 0) {
-            // LED Control
-            led_update(idx, proc.parseInt());
-        } else {
-            // Serial.println("Command Failed without return value");
-        }
 
-        // Serial.flush();
-        proc.flush();
-        memset(fp, 0, MAX_PATH);
+        // read data
+        read_led_data(fp, buf);
+
+        // control led when data is available
+        if (buf.length() != 0) {
+            led_update(idx, buf.toInt());
+        }
     }
 }
 
-void runDoorHandler()
-{
+void runDoorHandler() {
     /* dummy data */
     int door_inst = random(0, 5);
     /* dummy data */
 
-    // set path
-    char cmd[MAX_PATH]={0};
-    sprintf(cmd, "%s %d", door_sh_path, door_inst);
-
-    // run cat command
-    Process proc;
-    proc.runShellCommand(cmd);
-
-    // do nothing until the process finishes
-    while (proc.running());
-    
-    // Serial.flush();
-    proc.flush();
+    File door = FileSystem.open(door_path, FILE_WRITE);
+    if (door) {
+        door.println(door_inst);
+    }
+    door.close();
 }
 
 void loop() {
